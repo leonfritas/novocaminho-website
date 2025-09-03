e9603130c949f7521cc1783353de7fc5c12872bd
Leo <leon_fritas@hotmail.com>
2025-09-02 21:33:05 -0400

    adicionar seção de contato com informações e mapa, refatorar layout responsivo

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,8 +39,54 @@ export const Contact = (props) => {
       <div id="contact">
         <div className="container">
           <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
+            <div className="section-title">
+              <h2>Nosso Endereço</h2>
+            </div>
+            
+            <div className="row row-contact">
+              
+              <div className="map-container" style={{ marginTop: "40px" }}>
+                <iframe 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.8451632777533!2d-60.02512212425709!3d-3.1355752968398867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c057efa7909a3%3A0x8c6fc6669ad1e587!2sConj.%20Tocantins%2C%202229%20-%20Centro%2C%20Manaus%20-%20AM%2C%2069050-110!5e0!3m2!1spt-BR!2sbr!4v1756860015699!5m2!1spt-BR!2sbr" 
+                  width="600" 
+                  height="450" 
+                  style={{border:0, borderRadius: "20px"}} 
+                  allowFullScreen="" 
+                  loading="lazy" 
+                  referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
+              </div>
+              <div className="col-md-3 col-md-offset-1 contact-info">
+                <div className="contact-item">
+                  <h3>Informações </h3>
+                  <p>
+                    <span>
+                      <i className="fa fa-map-marker"></i> Endereço
+                    </span>
+                    {props.data ? "Rua Marcílio Dias nº 2229 Conjunto Tocantins - Centro" : "loading"}
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <p>
+                    <span>
+                      <i className="fa fa-phone"></i> Telefone
+                    </span>{" "}
+                    {props.data ? "(92) 98409-2632" : "loading"}
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <p>
+                    <span>
+                      <i className="fa fa-envelope-o"></i> E-mail
+                    </span>{" "}
+                    {props.data ? props.data.email : "loading"}
+                  </p>
+                </div>
+              </div>
+              
+            </div>
+            <div className="section-title">
+
                 <h2>Entre em contato</h2>
                 <p>
                   Preencha o formulário abaixo para nos enviar uma mensagem. 
@@ -96,35 +142,8 @@ export const Contact = (props) => {
                   Enviar Mensagem
                 </button>
               </form>
-            </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Informações de Contato</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Endereço
-                </span>
-                {props.data ? "Rua Cremona nº 357 Alvorada 2" : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Telefone
-                </span>{" "}
-                {props.data ? "(92) 99410-7470" : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> E-mail
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div>
+          
           <div className="col-md-12">
             <div className="row">
               <div className="social">
@@ -150,6 +169,7 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
+
       <div id="footer">
         <div className="container text-center">
           <h3          
